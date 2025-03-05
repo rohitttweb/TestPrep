459377eb6ea41d6519f67e90f161f2afd78ae18e
rohitttweb <rohitmehra3216@gmail.com>
2025-03-05 16:53:26 +0530

    second commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import EnvironmentPlugin from "vite-plugin-environment";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
 
-  plugins: [
-    react(),
-    EnvironmentPlugin({
-      API_URL: import.meta.env.VITE_API_URL || "http://192.168.1.25:3001" // Provide a fallback value
-    })
-  ],
+  
 })
